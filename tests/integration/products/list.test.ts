25ff3b8810fc2a81892bc89a361710a5a3413b9e
Bárbara <babivaldez@gmail.com>
2023-09-15 20:13:01 -0300

    Finaliza os testes do requisito 1

--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -1,10 +1,24 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
+import ProductModel from '../../../src/database/models/product.model';
+import listaDeProdutosFindAll from '../../mocks/listaDeProdutos';
 
 chai.use(chaiHttp);
 
 describe('GET /products', function () { 
   beforeEach(function () { sinon.restore(); });
+  it('Verifica se uma lista de produtos é retornada', async function () {
+    sinon.stub(ProductModel, 'findAll').resolves([
+      ProductModel.build({ "id": 1, "name": "Excalibur", "price": "10 peças de ouro", "orderId": 1 }),
+      ProductModel.build({ "id": 2, "name": "Espada Justiceira", "price": "20 peças de ouro", "orderId": 1 }),
+      ProductModel.build({ "id": 3, "name": "Lira de Orfeu", "price": "1 peça de ouro", "orderId": 2 }),
+    ]);
 
+    const response = await chai.request(app).get('/products');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(listaDeProdutosFindAll);
+  });
 });
---
+++ tests/mocks/listaDeProdutos.ts
@@ -0,0 +1,22 @@
+const listaDeProdutosFindAll = [
+    {
+      "id": 1,
+      "name": "Excalibur",
+      "price": "10 peças de ouro",
+      "orderId": 1
+    },
+    {
+      "id": 2,
+      "name": "Espada Justiceira",
+      "price": "20 peças de ouro",
+      "orderId": 1
+    },
+    {
+      "id": 3,
+      "name": "Lira de Orfeu",
+      "price": "1 peça de ouro",
+      "orderId": 2
+    },
+  ]
+
+  export default listaDeProdutosFindAll;
